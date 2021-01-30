e76ce5390ecb44a2106c5e34ad7200b0b67bfafc
khuzha <mr.khuzha@gmail.com>
2021-01-30 06:34:57 +0300

    norme: printf.h. Tests ok

--- printf.h
+++ printf.h
@@ -1,15 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zskeeter <zskeeter@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/30 06:19:06 by zskeeter          #+#    #+#             */
+/*   Updated: 2021/01/30 06:33:53 by zskeeter         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PRINTF_H
 # define PRINTF_H
 
-// DELETE:
-#include <stdio.h>
-// end of del
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include "libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
-typedef struct data
+typedef	struct	s_data
 {
 	char	type;
 	int		flag_minus;
@@ -18,32 +28,32 @@ typedef struct data
 	int		acc_no_value;
 	int		width;
 	int		acc;
-} t_data;
+}				t_data;
 
-void		print_c(struct data *data, char c, int *count);
-void		print_d(struct data *data, int value, int *count);
-void		print_s(struct data *data, char *str, int *count);
-void		print_u(struct data *data, unsigned int value, int *count);
-void		print_p(struct data *data, unsigned long long value, int *count);
-void		print_x(struct data *data, unsigned int value, int *count, int big_letters);
-// int		print_final(struct data *data, char *str, int len, int *count);
-int		ft_printf(const char *format, ...);
-int		count_digits(long n);
-char	*ft_itoa_16(unsigned int value, int big_letters);
-char	*ft_itoa_for_p(unsigned long long value);
-int		ft_max_int(int a, int b);
-
-void	print_data(struct data *data);
-void	ft_putstr_count(char *s, int *count);
-void	ft_putchar_count(char c, int *count);
-char	*ft_itoa_u(unsigned int n);
-int		is_type(char c);
-int		pos_or_zero(int num);
-
-int		is_format_char(char c);
-void	initialize_data(t_data *data);
-void	handle_if_type(char c, struct data *data, int *is_type);
-int		parse_acc_or_w_simple(char *format, struct data *data, int is_acc);
-void	parse_acc_or_w_varg(struct data *data, int is_acc, va_list ap);
+void			print_c(t_data *data, char c, int *count);
+void			print_d(t_data *data, int value, int *count);
+void			print_s(t_data *data, char *str, int *count);
+void			print_u(t_data *data, unsigned int value, int *count);
+void			print_p(t_data *data,
+					unsigned long long value, int *count);
+void			print_x(t_data *data,
+					unsigned int value, int *count, int big_letters);
+int				ft_printf(const char *format, ...);
+int				count_digits(long n);
+char			*ft_itoa_16(unsigned int value, int big_letters);
+char			*ft_itoa_for_p(unsigned long long value);
+int				ft_max_int(int a, int b);
+void			print_data(t_data *data);
+void			ft_putstr_count(char *s, int *count);
+void			ft_putchar_count(char c, int *count);
+char			*ft_itoa_u(unsigned int n);
+int				is_type(char c);
+int				pos_or_zero(int num);
+int				is_format_char(char c);
+void			initialize_data(t_data *data);
+void			handle_if_type(char c, t_data *data, int *is_type);
+int				parse_acc_or_w_simple(char *format,
+					t_data *data, int is_acc);
+void			parse_acc_or_w_varg(t_data *data, int is_acc, va_list ap);
 
 #endif
