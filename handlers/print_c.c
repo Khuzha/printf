62130192f7cc69a491888ab0193e65dd20fdaf1c
khuzha <mr.khuzha@gmail.com>
2021-01-30 04:12:07 +0300

    744/744 pft ok

--- handlers/print_c.c
+++ handlers/print_c.c
@@ -29,18 +29,32 @@ static void	make_str(struct data *data, char c, int *count)
 	if (!(str = malloc(sizeof(char) * (full_len + 1))))
 		return ;
 	inite_str(data, str, full_len + 1);
-	str[data->flag_minus ? 0 : full_len - 1] = c;
-	ft_putstr_count(str, count);
-	count++;
+	if (c)
+	{
+		str[data->flag_minus ? 0 : full_len - 1] = c;
+		ft_putstr_count(str, count);
+	}
+	else if (data->flag_minus && full_len > 1)
+	{
+		ft_putchar_count(0, count);
+		ft_putstr_count(&str[1], count);
+	}
+	else if (!data->flag_minus && full_len > 1)
+	{
+		ft_putstr_count(&str[1], count);
+		ft_putchar_count(0, count);
+	}
+	else
+		ft_putchar_count(0, count);
 	free(str);
 }
 
 void			print_c(struct data *data, char c, int *count)
 {
-	if (c == 0)
-	{
-		ft_putchar_count(0, count);
-	}
+	// if (c == 0)
+	// {
+	// 	ft_putchar_count(0, count);
+	// }
 	data->apply_acc = 0;
 	make_str(data, c, count);
 }
